16b804ee1d892dad9a0b5bf2da28b7cc4cd4d119
Daniel <nmsbnmsb1@126.com>
2024-09-20 16:56:05 +0700

    fix: headers cannot be set (#12)
    Co-authored-by: TinsFox <fox@tinsfox.com>

--- src/lib/api-fetch.ts
+++ src/lib/api-fetch.ts
@@ -3,6 +3,23 @@ import { FetchError, ofetch } from "ofetch"
 export const apiFetch = ofetch.create({
   credentials: "include",
   retry: false,
+  onRequest: async ({ options }) => {
+    const header = new Headers(options.headers)
+
+    options.headers = header
+
+    if (options.method && options.method.toLowerCase() !== "get") {
+      if (typeof options.body === "string") {
+        options.body = JSON.parse(options.body)
+      }
+      if (!options.body) {
+        options.body = {}
+      }
+    }
+  },
+  onResponse() {
+    // TODO: response interceptor
+  },
   onResponseError(context) {
     if (context.response.status === 401) {
       return redirectToLogin()
