f84c8942b5aed59991426bb28616bcf2313ee9fe
TinsFox <fox@tinsfox.com>
2025-05-05 13:52:07 +0800

    fix: replace env variable with process.env for DATABASE_URL in drizzle.config.ts

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,13 +1,13 @@
 import { config } from "dotenv";
 config({ path: "./.dev.vars", debug: true });
+import * as process from "node:process";
 import { defineConfig } from "drizzle-kit";
-import { env } from "env";
 
 export default defineConfig({
 	schema: ["./src/db/schema/**/*.ts", "./auth-schema.ts"],
 	out: "./drizzle",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: env.DATABASE_URL,
+		url: process.env.DATABASE_URL,
 	},
 });
