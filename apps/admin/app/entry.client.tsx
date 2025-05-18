a97542320ffdb15c7d61f0c01b6eaeed8e9773ee
TinsFox <fox@tinsfox.com>
2025-05-18 18:29:45 +0800

    refactor: simplify entry point by removing RootProviders wrapper

--- apps/admin/app/entry.client.tsx
+++ apps/admin/app/entry.client.tsx
@@ -1,15 +1,12 @@
 import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
-import { RootProviders } from "./providers/root-providers";
 
 startTransition(() => {
 	hydrateRoot(
 		document,
 		<StrictMode>
-			<RootProviders>
-				<HydratedRouter />
-			</RootProviders>
+			<HydratedRouter />
 		</StrictMode>,
 	);
 });
